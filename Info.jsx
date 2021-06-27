3c38aa1bc2ef8ccf1d93a1696e8e0fcd8561d74c
b171284 <86551878+b171284@users.noreply.github.com>
2021-06-27 19:07:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Info(){
+  return(
+    <div className="note">
+      <h1>7 DAY LONG FREE PYTHON 
+        AND NATURAL LANGUAGE PROCESSING
+         BOOTCAMP
+      </h1>
+      <p>*starts 27th june...
+        *certificates........
+        *hands on projects...
+      </p>
+      </div>
+);
+}
+export default Info;
---
+++ Info1.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+function Info1(){
+  return(
+    <div className="note">
+      <h1>7 DAY LONG FREE PYTHON 
+        AND NETWORK SECURITY
+         BOOTCAMP
+      </h1>
+      <p>
+        *starts 27th june...
+        *certificates........
+        *hands on projects...
+        *no prerequisites
+        </p>
+      </div>
+);
+}
+export default Info1;
